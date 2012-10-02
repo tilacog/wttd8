8c38023d4ef4ebe3b086fb981b99f5d932c2bf15
Tiago Guimaraes <tilacog@gmail.com>
2012-10-02 11:04:55 -0300

    Funcionando até o slide 83

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,9 +1,10 @@
 #coding: utf-8
 from django import forms
+from django.utils.translation import gettext as _
 
 
 class SubscriptionForm(forms.Form):
-    name    =   forms.CharField()
-    cpf     =   forms.CharField()
-    email   =   forms.EmailField()
-    phone   =   forms.CharField()
+    name    =   forms.CharField(label=_( 'Nome'    ))
+    cpf     =   forms.CharField(label=_( 'CPF'     ))
+    email   =   forms.EmailField(label=_('E-mail'  ))
+    phone   =   forms.CharField(label=_( 'Telefone'))
