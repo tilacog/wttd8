7541ca653d2c44f7ffd3c563691c5ec99e470c57
Tiago Guimaraes <tilacog@gmail.com>
2012-10-02 10:35:23 -0300

    Funcionando até o slide 75

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,11 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+#coding: utf-8
 
 from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscribeTest(TestCase):
     def setUp(self):
@@ -23,6 +19,20 @@ class SubscribeTest(TestCase):
     def test_html(self):
         'Html must contain input controls'
         self.assertContains(self.resp, '<form')
-        self.assertContains(self.resp, '<input', 5)
+        self.assertContains(self.resp, '<input', 6)
         self.assertContains(self.resp, 'type="text"', 4)
         self.assertContains(self.resp, 'type="submit"')
+    
+    def test_csrf(self):
+        'Html musc contain csrf token.'
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+    
+    def test_has_forms(self):
+        'Context must have subscription form.'
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+    
+    def test_form_has_fields(self):
+        'Form must have 4 fields'
+        form = self.resp.context['form']
+        self.assertItemsEqual(['name','email','cpf','phone'], form.fields)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,7 +1,9 @@
 #coding: utf-8
 
 from django.views.generic.simple import direct_to_template
-
+from eventex.subscriptions.forms import SubscriptionForm
 
 def subscribe(request):
-    return direct_to_template(request, 'subscriptions/subscription_form.html')
+    return direct_to_template(request, 'subscriptions/subscription_form.html',
+                                {'form':SubscriptionForm})
+
