f00f1ab2e66768351e799f4ff94843b449725c50
Tiago Guimaraes <tilacog@gmail.com>
2012-10-11 16:31:58 -0300

    Funcionando até o S11A03

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -5,13 +6,14 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'eventex.core.views.homepage', name='homepage'),
-    url(r'inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
-    url(r'inscricao/(\d+)/$', 'eventex.subscriptions.views.success', name='success'),
+    url(r'', include('eventex.core.urls', namespace='core')),
+    url(r'inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
     
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'', include('eventex.core.urls', namespace='core')),
 )
