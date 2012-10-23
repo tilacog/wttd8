19e9bb491d3f91f446d0b5ed245e9e542bda2e12
Tiago Guimaraes <tilacog@gmail.com>
2012-10-22 23:48:33 -0200

    Funcionando até S145A03

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,9 +1,23 @@
 #coding: utf-8
 from django import forms
+from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
 from eventex.subscriptions.models import Subscription
 
+
+
+
+
+def CPFValidator(value):
+    if not value.isdigit():
+        raise ValidationError(_('CPF deve conter apenas números.'))
 
 class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = Subscription
         exclude = ('paid',)
+
+    def __init__(self, *args, **kwargs):
+        super(SubscriptionForm, self).__init__(*args, **kwargs)
+        
+        self.fields['cpf'].validators.append(CPFValidator)
--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -12,3 +12,13 @@ class SubscriptionFormTest(TestCase):
         'Form must have 4 fields'
         form = self.resp.context['form']
         self.assertItemsEqual(['name','email','cpf','phone'], form.fields)
+    
+    def test_cpf_is_digit(self):
+        'CPF must only accept digits.'
+        data = dict(name='Tiago',email='tiago@gmail.com',
+                    cpf ='12345678901', phone='34414829')
+        data.update({'cpf':'ABCD5678901'})
+        form = SubscriptionForm(data)
+        form.is_valid()
+
+        self.assertItemsEqual(['cpf'], form.errors)
