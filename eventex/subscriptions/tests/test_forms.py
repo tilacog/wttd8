8ca9aaf261af92d758f733391bc2f8b87d649676
Tiago Guimaraes <tilacog@gmail.com>
2012-10-23 00:06:05 -0200

    Funcionando até S154A03

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -15,22 +15,19 @@ class SubscriptionFormTest(TestCase):
     
     def test_cpf_is_digit(self):
         'CPF must only accept digits.'
-        data = dict(name='Tiago',email='tiago@gmail.com',
-                    cpf ='12345678901', phone='34414829')
-        data.update({'cpf':'ABCD5678901'})
-        form = SubscriptionForm(data)
-        form.is_valid()
+        form = self.make_validated_form(cpf='ABCD5678901')
         self.assertItemsEqual(['cpf'], form.errors)
 
     def test_cpf_has_11_digits(self):
         'CPF must have 11 digits'
-        data = dict(name='Tiago',email='tiago@gmail.com',
-                    cpf ='12345678901', phone='34414829')
-        data.update({'cpf':'1234'})
-        form = SubscriptionForm(data)
-        form.is_valid()
+        form = self.make_validated_form(cpf='1234')
         self.assertItemsEqual(['cpf'], form.errors)
 
-      
-
+    def make_validated_form(self, **kwargs):
+        data = dict(name='Tiago',email='tiago@gmail.com',
+                    cpf ='12345678901', phone='34414829')
+        data.update(kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
 
